d8283caa0ceb4789f35cf273a9ef4fbfdf53b377
lucas <lucasnotaroberto2@gmail.com>
2026-04-22 16:46:56 -0300

    qualquer coisa

--- fullstack_aulas/servidor/server.js
+++ fullstack_aulas/servidor/server.js
@@ -1,2 +1,8 @@
-require("colors")
-console.log("olá,mundo".rainbow)
+require("colors");
+var http = require("http");
+var express = require("express");
+var app = express();
+app.use(express.static("./public"));
+var server = http.createServer(app);
+server.listen(3000);
+console.log("servidor rodando...".rainbow);
